f4452ea6934456ffbf5632868ca3e502032631cf
Hamza6890 <hamzaalivr400@gmail.com>
2024-07-14 02:03:41 -0700

    update appbar.dart file

--- practice/lib/appbar.dart
+++ practice/lib/appbar.dart
@@ -1,24 +1,43 @@
 import 'package:flutter/material.dart';
 
 class appBar_section extends StatelessWidget {
-  const appBar_section({super.key});
+   appBar_section({super.key});
+  TextEditingController searchCotroller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-        body: Container(
+      body: Container(
             height: 50,
             width: MediaQuery.of(context).size.width,
-            color: Colors.blue,
-          child: Row(
+            color: Colors.transparent,
+        child: Row(
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
+          children: [
               SizedBox(width: 20,),
               Icon(Icons.qr_code_scanner,color: Colors.black,),
-              
-            ],
-          ),
-          ),
+              SizedBox(width: 20,),
+              Container(
+                color: Colors.white,
+                height: 30,
+                width: 320,
+                child: TextFormField(
+                  controller: searchCotroller,
+                  decoration: InputDecoration(
+                    labelStyle: TextStyle(color: Colors.black,),
+                    hintText: "search....",
+                      suffixIcon: Icon(Icons.search,color: Colors.black,),
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(0)),
+                      contentPadding: EdgeInsets.all(10),
+                  ),
+                ),
+              ),SizedBox(width: 20,),
+              Icon(Icons.notifications,color: Colors.black,),
+              SizedBox(width: 20,),
+              Icon(Icons.account_balance_wallet_rounded,color: Colors.black,),
+          ],
+        ),
+      ),
     );
   }
 }
