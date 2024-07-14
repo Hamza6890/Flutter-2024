231b99abf4134cb60eadae924570b6175852a7e3
Hamza6890 <hamzaalivr400@gmail.com>
2024-07-14 05:20:47 -0700

    update

--- daraz_ui/lib/dashboard.dart
+++ daraz_ui/lib/dashboard.dart
@@ -1,5 +1,9 @@
 import 'package:daraz_ui/slider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'flash_sale.dart';
+import 'location.dart';
 
 class Dashboard_ui extends StatelessWidget {
   const Dashboard_ui({super.key});
@@ -10,50 +14,73 @@ class Dashboard_ui extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.white,
+          backgroundColor: Colors.transparent,
           title: Row(
             children: [
               Column(
                 children: [
-                  Icon(Icons.qr_code_scanner,color: Colors.black,),
-                  Text("Scan",style: TextStyle(
-                    fontSize: 10
-                  ),)
+                  Icon(
+                    Icons.qr_code_scanner,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    "Scan",
+                    style: TextStyle(fontSize: 10),
+                  )
                 ],
               ),
-              SizedBox(width: 20,),
+              SizedBox(
+                width: 20,
+              ),
               Container(
                 width: MediaQuery.of(context).size.width / 1.6,
                 decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10)
-                ),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
                 child: TextFormField(
                   decoration: InputDecoration(
-                    labelStyle: TextStyle(color: Colors.black,),
+                    labelStyle: TextStyle(
+                      color: Colors.black,
+                    ),
                     hintText: "search....",
-                    suffixIcon: Icon(Icons.search,color: Colors.black,),
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                    suffixIcon: Icon(
+                      Icons.search,
+                      color: Colors.black,
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
                     contentPadding: EdgeInsets.all(10),
                   ),
                 ),
               ),
-              SizedBox(width: 20,),
+              SizedBox(
+                width: 20,
+              ),
               Column(
                 children: [
-                  Icon(Icons.currency_bitcoin,color: Colors.black,),
-                  Text("Coin",style: TextStyle(
-                      fontSize: 10
-                  ),)
+                  Icon(
+                    Icons.currency_bitcoin,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    "Coin",
+                    style: TextStyle(fontSize: 10),
+                  )
                 ],
               ),
-              SizedBox(width: 20,),
+              SizedBox(
+                width: 20,
+              ),
               Column(
                 children: [
-                  Icon(Icons.account_balance_wallet_outlined,color: Colors.black,),
-                  Text("Wallet",style: TextStyle(
-                      fontSize: 10
-                  ),)
+                  Icon(
+                    Icons.account_balance_wallet_outlined,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    "Wallet",
+                    style: TextStyle(fontSize: 10),
+                  )
                 ],
               ),
             ],
@@ -67,22 +94,62 @@ class Dashboard_ui extends StatelessWidget {
                   Container(
                       height: 200,
                       width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                          color: Colors.amberAccent
-                      ),
-                      child: ImageSlider()
-                  ), //Slider Container
-                  Container(
-                    height: 50,
-                    width: MediaQuery.of(context).size.width,
-                    margin: EdgeInsets.only(left: 20,right: 20,top: 190),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: ,
-                        borderRadius: BorderRadius.circular(20)
-                    ),
-                  )
+                      decoration: BoxDecoration(color: Colors.amberAccent),
+                      child: ImageSlider()), //Slider Container
+                  Location_Container(),
                 ],
+              ),
+              Container(
+                height: 250,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  children: [
+                    flash_sale_container(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: 180,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            color: Colors.red
+                          ),
+                        ),
+                        Container(
+                          height: 180,
+                          width: 120,
+                          decoration: BoxDecoration(
+                              color: Colors.amber
+                          ),
+                        ),
+                        Container(
+                          height: 180,
+                          width: 120,
+                          decoration: BoxDecoration(
+                              color: Colors.red
+                          ),
+                        ),
+                        Container(
+                          height: 180,
+                          width: 120,
+                          decoration: BoxDecoration(
+                              color: Colors.amber
+                          ),
+                        ),
+                        Container(
+                          height: 180,
+                          width: 120,
+                          decoration: BoxDecoration(
+                              color: Colors.red
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ],
           ),
